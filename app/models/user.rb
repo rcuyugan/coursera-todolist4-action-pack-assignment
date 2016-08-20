5b83a8fa66c2d1a9ea623d5a73be9ec137c66f91
Robert Cuyugan <tebsbmr05@gmail.com>
2016-08-19 22:26:30 -0700

    od4 changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 	has_many :todo_lists, dependent: :destroy
 	has_many :todo_items, through: :todo_lists, source: :todo_items
 	validates :username, presence: true
+	has_secure_password
 
 	def get_completed_count
 	 TodoItem.joins(:todo_list).where(completed: "t").count
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: "todo_lists#index"
+
+  resources :todo_lists do
+    resources :todo_items
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
